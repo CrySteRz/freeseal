781961dec12629f93befcc518abc68339ebfea65
CrySteRz <blidaruionutz@gmail.com>
2024-06-19 19:14:25 +0300

    Fixed api errors and errors controller

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -7,40 +7,34 @@ class ErrorsController < ActionController::Base
   ].freeze
 
   def show
-    return unless error_status_code == 404
+    respond_to do |format|
+      format.json do
+        set_cors_headers
 
-    render json: { status: 404, message: 'Not Found' }, status: :not_found
-  end
+        exception = request.env['action_dispatch.exception']
+        error = exception.message if exception.class.in?(SAFE_ERROR_MESSAGE_CLASSES)
 
+        render json: { status: error_status_code, error: error }.compact, status: error_status_code
+      end
 
-  respond_to do |f|
-    f.json do
-      set_cors_headers
-
-      exception = request.env['action_dispatch.exception']
-
-      error = exception.message if exception.class.in?(SAFE_ERROR_MESSAGE_CLASSES)
-
-      render json: { status: error_status_code, error: }.compact, status: error_status_code
+      format.html do
+        render template: "errors/#{error_status_code}", status: error_status_code
+      end
     end
-
-    f.html { render error_status_code.to_s, status: error_status_code }
   end
-end
 
   private
 
-def set_cors_headers
-  headers['Access-Control-Allow-Origin'] = '*'
-  headers['Access-Control-Allow-Methods'] = 'POST, GET, PUT, PATCH, DELETE, OPTIONS'
-  headers['Access-Control-Allow-Headers'] = '*'
-  headers['Access-Control-Max-Age'] = '1728000'
-  headers['Access-Control-Allow-Credentials'] = true
-end
+  def set_cors_headers
+    headers['Access-Control-Allow-Origin'] = '*'
+    headers['Access-Control-Allow-Methods'] = 'POST, GET, PUT, PATCH, DELETE, OPTIONS'
+    headers['Access-Control-Allow-Headers'] = '*'
+    headers['Access-Control-Max-Age'] = '1728000'
+    headers['Access-Control-Allow-Credentials'] = true
+  end
 
-def error_status_code
-  @error_status_code ||=
-    ActionDispatch::ExceptionWrapper.new(request.env,
-                                         request.env['action_dispatch.exception']).status_code
+  def error_status_code
+    @error_status_code ||=
+      ActionDispatch::ExceptionWrapper.new(request.env, request.env['action_dispatch.exception']).status_code
+  end
 end
-
